55a0c9a654bff0637baad39f28ed17a31aa8aa10
Ilyass cherradi <ilyasscherradi15@gmail.com>
2026-03-06 16:28:42 +0100

    build:implement generate

--- src/main.c
+++ src/main.c
@@ -17,7 +17,8 @@ int main()
         printf("\nMenu:\n");
         printf("1. Charger un automate a partir d'un fichier .dot\n");
         printf("2. Afficher l'automate\n");
-        printf("3. Quitter\n");
+        printf("3. Generer un fichier .dot a partir de l'automate actuel\n");
+        printf("4. Quitter\n");
         printf("Votre choix : ");
 
         if (scanf("%d", &choix) != 1)
@@ -55,7 +56,13 @@ int main()
                 printf("Veuillez d'abord charger un automate (Option 1).\n");
             }
             break;
-        case 3:
+             case 3:
+            printf("Entrez le nom du fichier de sortie pour le .dot genere (ex: output.dot) : ");
+            scanf(" %255[^\n\r]", filename);
+            generate_dot(automate, "filename");
+            printf("Automate genere dans le fichier '%s'.\n", filename);
+            break;
+        case 4:
             printf("Au revoir !\n");
             free_automaton(automate);
             return 0;
